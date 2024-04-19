8f5220adf740966ac0649fb552758f67aa124b83
fc58165 <fc58165@alunos.ciencias.ulisboa.pt>
2024-04-19 13:51:04 +0100

    added site-list component skeleton

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SiteListComponent } from './site-list/site-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/site-list/site-list.component.ts
@@ -0,0 +1,26 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { SiteListDataSource, SiteListItem } from './site-list-datasource';
+
+@Component({
+  selector: 'app-site-list',
+  templateUrl: './site-list.component.html',
+  styleUrls: ['./site-list.component.css']
+})
+export class SiteListComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<SiteListItem>;
+  dataSource = new SiteListDataSource();
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
