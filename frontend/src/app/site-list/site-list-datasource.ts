fd662a616203d4465b74135c90a6ef66a218bd46
fc58165 <fc58165@alunos.ciencias.ulisboa.pt>
2024-04-28 20:27:48 +0100

    refactored site to use MatTableDataSource() instead

--- frontend/src/app/site-list/site-list-datasource.ts
+++ frontend/src/app/site-list/site-list-datasource.ts
@@ -4,6 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
 import { Website, RatingStatus, RatingResult, Page } from '../website';
+import { MatTableDataSource } from '@angular/material/table';
 
 // TODO: replace this with real data from your application
 const TIME_1 = new Date('December 15, 2024 04:28:00');	
@@ -34,7 +35,8 @@ const EXAMPLE_SITES: Website[] = [
  * (including sorting, pagination, and filtering).
  */
 export class SiteListDataSource extends DataSource<Website> {
-  data: Website[] = EXAMPLE_SITES;
+  // data: Website[] = EXAMPLE_SITES;
+  dataSource = new MatTableDataSource(EXAMPLE_SITES);
   paginator: MatPaginator | undefined;
   sort: MatSort | undefined;
 
@@ -51,9 +53,9 @@ export class SiteListDataSource extends DataSource<Website> {
     if (this.paginator && this.sort) {
       // Combine everything that affects the rendered data into one update
       // stream for the data-table to consume.
-      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
+      return merge(observableOf(this.dataSource), this.paginator.page, this.sort.sortChange)
         .pipe(map(() => {
-          return this.getPagedData(this.getSortedData([...this.data ]));
+          return this.getPagedData(this.getSortedData([...this.dataSource.data ]));
         }));
     } else {
       throw Error('Please set the paginator and sort on the data source before connecting.');
@@ -103,3 +105,75 @@ export class SiteListDataSource extends DataSource<Website> {
 function compare(a: string | number, b: string | number, isAsc: boolean): number {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
+
+// export class SiteListDataSource extends DataSource<Website> {
+//   // data: Website[] = EXAMPLE_SITES;
+//   dataSource = new MatTableDataSource(EXAMPLE_SITES);
+//   paginator: MatPaginator | undefined;
+//   sort: MatSort | undefined;
+
+//   constructor() {
+//     super();
+//   }
+
+//   /**
+//    * Connect this data source to the table. The table will only update when
+//    * the returned stream emits new items.
+//    * @returns A stream of the items to be rendered.
+//    */
+//   connect(): Observable<Website[]> {
+//     if (this.paginator && this.sort) {
+//       // Combine everything that affects the rendered data into one update
+//       // stream for the data-table to consume.
+//       return merge(observableOf(this.dataSource), this.paginator.page, this.sort.sortChange)
+//         .pipe(map(() => {
+//           return this.getPagedData(this.getSortedData([...this.dataSource.data ]));
+//         }));
+//     } else {
+//       throw Error('Please set the paginator and sort on the data source before connecting.');
+//     }
+//   }
+
+//   /**
+//    *  Called when the table is being destroyed. Use this function, to clean up
+//    * any open connections or free any held resources that were set up during connect.
+//    */
+//   disconnect(): void {}
+
+//   /**
+//    * Paginate the data (client-side). If you're using server-side pagination,
+//    * this would be replaced by requesting the appropriate data from the server.
+//    */
+//   private getPagedData(data: Website[]): Website[] {
+//     if (this.paginator) {
+//       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+//       return data.splice(startIndex, this.paginator.pageSize);
+//     } else {
+//       return data;
+//     }
+//   }
+
+//   /**
+//    * Sort the data (client-side). If you're using server-side sorting,
+//    * this would be replaced by requesting the appropriate data from the server.
+//    */
+//   private getSortedData(data: Website[]): Website[] {
+//     if (!this.sort || !this.sort.active || this.sort.direction === '') {
+//       return data;
+//     }
+
+//     return data.sort((a, b) => {
+//       const isAsc = this.sort?.direction === 'asc';
+//       switch (this.sort?.active) {
+//         case 'addedDate': return compare(+a.addedDate, +b.addedDate, isAsc);
+//         case 'lastEvalDate': return compare(+a.lastEvalDate, +b.lastEvalDate, isAsc);
+//         default: return 0;
+//       }
+//     });
+//   }
+// }
+
+// /** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+// function compare(a: string | number, b: string | number, isAsc: boolean): number {
+//   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+// }
