8da47c52c1aa6dee090ba1042108c6fcd74c9cdb
Filipe da Costa <fc55549@alunos.ciencias.ulisboa.pt>
2024-04-28 22:53:34 +0100

    feat: Implement more website service functions

--- frontend/src/app/website.service.ts
+++ frontend/src/app/website.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Website } from './website';
+import { Page, Website } from './website';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsiteService {
 
-  private url = 'api/website';
+  private websiteURL = 'api/website';
+  private websitesURL = 'api/websites';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
   constructor(private http: HttpClient) {}
 
   getWebsiteById(id: string): Observable<Website> {
-    const url = `${this.url}/${id}`;
-    return this.http.get<Website>(url)
+    const url = `${this.websiteURL}/${id}`;
+    return this.http.get<Website>(url);
+  }
+
+  getWebsites(): Observable<Website[]> {
+    return this.http.get<Website[]>(this.websitesURL);
+  }
+
+  addWebsite(website: Website): Observable<Website> {
+    return this.http.post<Website>(this.websitesURL, website, this.httpOptions);
+  }
+
+  addPageToWebsite(page: Page, websiteId: string) {
+    const url = `${this.websiteURL}/${websiteId}`;
+    return this.http.post<Page>(url, page, this.httpOptions);
   }
 }
