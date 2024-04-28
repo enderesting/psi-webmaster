501ca48b443458dda517c9dd3106f3372ece4d7a
Marta <marta.lou120@gmail.com>
2024-04-28 09:59:43 +0100

    page being correctly displayed

--- frontend/src/app/page-list/page-list.component.ts
+++ frontend/src/app/page-list/page-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, Input } from '@angular/core';
 import { MatTable } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -11,17 +11,15 @@ import { Website, RatingStatus, RatingResult, Page } from '../website';
   styleUrls: ['./page-list.component.css']
 })
 export class PageListComponent implements AfterViewInit {
+  @Input() monitoredPages: Page[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<Page>;
-  dataSource = new PageListDataSource();
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['websiteURL','lastEvalDate','ratingResult'];
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    this.table.dataSource = this.monitoredPages;
   }
 }
