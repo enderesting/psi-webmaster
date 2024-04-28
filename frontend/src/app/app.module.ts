1c302e4a3a8caab61877542245fca254b8a9e39c
Marta <marta.lou120@gmail.com>
2024-04-28 11:16:01 +0100

    created website-service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -34,7 +35,8 @@ import { AppRoutingModule } from './app-routing.module';
         MatListModule,
         StatusFilterComponent,
         CommonModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
     ]
 })
 export class AppModule { }
