e98a92ce2964823bc74b1115572427c6ca791ff4
Filipe da Costa <fc55549@alunos.ciencias.ulisboa.pt>
2024-05-11 21:25:51 +0100

    feat: Implement request rating endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,8 +6,11 @@ const websiteController = require('../controllers/website');
 
 router.get("/websites", websitesController.getWebsites);
 router.get("/website/:id", websiteController.getWebsite);
+
 router.post("/websites", websitesController.addWebsite);
 router.post("/website/:id", websiteController.addPage);
+router.post("/website/:id/evaluate", websiteController.requestRating);
+
 router.delete("/page/:id", websiteController.deletePage);
 
 module.exports = router;
