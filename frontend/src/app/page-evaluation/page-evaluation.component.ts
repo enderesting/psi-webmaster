d9f494c9109dc31b3083c645d92ac425d909053f
Marta <marta.lou120@gmail.com>
2024-05-15 08:55:42 +0100

    added getPageEvaluationById service

--- frontend/src/app/page-evaluation/page-evaluation.component.ts
+++ frontend/src/app/page-evaluation/page-evaluation.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Website, RatingStatus, RatingResult, Page, ErrorElement} from '../website';
+import { WebsiteService } from '../website.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-page-evaluation',
@@ -7,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class PageEvaluationComponent {
 
+  page!: Page;
+
+  constructor(
+    private route: ActivatedRoute,
+    private websiteService: WebsiteService,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.websiteService.getPageEvaluationById(id)
+      .subscribe(page => {
+        this.page = page
+      });
+  }
 }
