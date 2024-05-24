1608093e980116384053ee4322368ccff92566c6
Marta <marta.lou120@gmail.com>
2024-05-24 14:38:40 +0100

    functional filters

--- frontend/src/app/page-evaluation/page-evaluation.component.ts
+++ frontend/src/app/page-evaluation/page-evaluation.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Website, RatingStatus, RatingResult, Page, ErrorElement, QWAssertion} from '../website';
 import { WebsiteService } from '../website.service';
 import { Location } from '@angular/common';
+import { MatChipListboxChange, MatChipOption } from '@angular/material/chips';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-page-evaluation',
@@ -13,6 +15,27 @@ export class PageEvaluationComponent {
 
   page!: Page;
   rules!: QWAssertion[];
+
+  mockRules: QWAssertion[] = [
+    {
+      code: 'rule1',
+      outcome: 'Passed',
+      description: 'Description for rule1',
+      levels: ['A'],
+      elementsAffected: ['element1', 'element2'],
+      module: 'act',
+      page: this.page
+    },
+    {
+      code: 'rule2',
+      outcome: 'Failed',
+      description: 'Description for rule2',
+      levels: ['AA', 'AAA'],
+      elementsAffected: ['element3'],
+      module: 'wcag',
+      page: this.page
+    }
+  ];
 
   passedPercentage!: number;
   warningPercentage!: number;
@@ -30,7 +53,10 @@ export class PageEvaluationComponent {
     this.websiteService.getPageEvaluationById(id)
       .subscribe(page => {
         this.page = page;
-        this.rules = page.rules ?? [];
+        //this.rules = page.rules ?? [];
+        this.rules = this.mockRules;
+
+        this.filterRules();
 
         this.passedPercentage = this.calculatePercentage(this.page?.totalPassed ?? 0);
         this.warningPercentage = this.calculatePercentage(this.page?.totalWarning ?? 0);
@@ -46,5 +72,43 @@ export class PageEvaluationComponent {
 
   goBack(): void {
     this.location.back();
+  }
+
+  modules = ['act', 'wcag'];
+  results = ['Passed', 'Warning', 'Failed', 'Not applicable'];
+  levels = ['A', 'AA', 'AAA'];
+
+  selectedModules: String[] = this.modules;
+  selectedResults: String[] = this.results;
+  selectedLevels: String[] = this.levels;
+
+  filteredRules: QWAssertion[] = [];
+
+  setFilter(event:MatChipListboxChange, filterType: string) {
+    var listbox: MatChipOption | MatChipOption[] = event.source.selected;
+
+    let chipOpts : MatChipOption[];
+    if (listbox instanceof MatChipOption)
+      chipOpts = [listbox];
+    else
+      chipOpts = listbox;
+
+    const values : String[] = chipOpts.map((opt) => opt.value);
+    if (filterType === 'module')
+      this.selectedModules = values;
+    else if (filterType === 'result')
+      this.selectedResults = values;
+    else if (filterType === 'level')
+      this.selectedLevels = values;
+
+    this.filterRules();
+  }
+
+  filterRules() {
+    this.filteredRules = this.rules.filter(rule => {
+      return this.selectedModules.includes(rule.module) &&
+        this.selectedResults.includes(rule.outcome) &&
+        rule.levels.some(level => this.selectedLevels.includes(level));
+    });
   }
 }
