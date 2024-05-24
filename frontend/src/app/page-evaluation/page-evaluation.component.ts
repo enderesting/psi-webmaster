8415f2ef92f0b5b04f85aec8458482d49d790102
Marta <marta.lou120@gmail.com>
2024-05-24 07:02:44 +0100

    added percentage calculation

--- frontend/src/app/page-evaluation/page-evaluation.component.ts
+++ frontend/src/app/page-evaluation/page-evaluation.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Website, RatingStatus, RatingResult, Page, ErrorElement} from '../website';
+import { Website, RatingStatus, RatingResult, Page, ErrorElement, QWAssertion} from '../website';
 import { WebsiteService } from '../website.service';
 import { Location } from '@angular/common';
 
@@ -12,6 +12,12 @@ import { Location } from '@angular/common';
 export class PageEvaluationComponent {
 
   page!: Page;
+  rules!: QWAssertion[];
+
+  passedPercentage!: number;
+  warningPercentage!: number;
+  failedPercentage!: number;
+  notApplicablePercentage!: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,7 +29,22 @@ export class PageEvaluationComponent {
     const id = this.route.snapshot.paramMap.get('id')!;
     this.websiteService.getPageEvaluationById(id)
       .subscribe(page => {
-        this.page = page
+        this.page = page;
+        this.rules = page.rules ?? [];
+
+        this.passedPercentage = this.calculatePercentage(this.page?.totalPassed ?? 0);
+        this.warningPercentage = this.calculatePercentage(this.page?.totalWarning ?? 0);
+        this.failedPercentage = this.calculatePercentage(this.page?.totalFailed ?? 0);
+        this.notApplicablePercentage = this.calculatePercentage(this.page?.totalNotApplicable ?? 0);
       });
+  }
+
+  private calculatePercentage(tests : number): number {
+    const totalTests = this.page?.totalTests ?? 1;
+    return (tests/totalTests) * 100;
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
